e9e33dd6b4e0e43a154f401fcac77cec1019d2e6
Jamie Twells <jtwells3@googlemail.com>
2018-04-11 19:33:48 +0100

    Add project files.

---
+++ PropertyPathToObject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PropertyPathToObject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
